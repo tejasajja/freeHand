d6837b876ca201c8f5d44a83e0b04d18b8e89d69
Teja Sajja <teja.sajja75@gmail.com>
2024-07-23 14:54:35 -0700

    noauth

--- src/components/draw/Cam.tsx
+++ src/components/draw/Cam.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useRef, useEffect, useState } from 'react';
 import Webcam from 'react-webcam';
 import { Hands, HAND_CONNECTIONS } from '@mediapipe/hands';
@@ -18,12 +17,12 @@ const Cam: React.FC = () => {
   const webcamRef = useRef<Webcam | null>(null);
   const videoCanvasRef = useRef<HTMLCanvasElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  
   const cursorRef = useRef<HTMLDivElement | null>(null);
   const drawHistory = useRef<ImageData[]>([]);
   const historyPointer = useRef<number>(0);
   const shouldDraw = useRef<boolean>(false);
   const [showVideoCanvas, setShowVideoCanvas] = useState<boolean>(true);
-  const [webcamAllowed, setWebcamAllowed] = useState<boolean>(false);
 
   const { activeMenuItem, actionMenuItem } = useSelector((state: RootState) => state.menu);
   const { color, size } = useSelector((state: RootState) => state.toolbox[activeMenuItem] || { color: '#000', size: 1 });
@@ -183,174 +182,160 @@ const Cam: React.FC = () => {
 
   // Hand detection setup
   useEffect(() => {
-    const setupHandDetection = async () => {
-      const handpose = new Hands({
-        locateFile: (file) => {
-          return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-        },
-      });
+    const handpose = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
 
-      handpose.setOptions({
-        maxNumHands: 2,
-        modelComplexity: 1,
-        minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5,
-      });
+    handpose.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
 
-      handpose.onResults((results) => {
-        const videoCanvasElement = videoCanvasRef.current;
-        const drawCanvasElement = canvasRef.current;
-        const videoCanvasCtx = videoCanvasElement?.getContext('2d');
+    handpose.onResults((results) => {
+      const videoCanvasElement = videoCanvasRef.current;
+      const drawCanvasElement = canvasRef.current;
+      const videoCanvasCtx = videoCanvasElement?.getContext('2d');
 
-        if (!videoCanvasElement || !videoCanvasCtx || !drawCanvasElement || !webcamRef.current) return;
+      if (!videoCanvasElement || !videoCanvasCtx || !drawCanvasElement || !webcamRef.current) return;
 
-        const videoWidth = webcamRef.current.video?.videoWidth || window.innerWidth;
-        const videoHeight = webcamRef.current.video?.videoHeight || window.innerHeight;
-        videoCanvasElement.width = videoWidth;
-        videoCanvasElement.height = videoHeight;
+      const videoWidth = webcamRef.current.video?.videoWidth || window.innerWidth;
+      const videoHeight = webcamRef.current.video?.videoHeight || window.innerHeight;
+      videoCanvasElement.width = videoWidth;
+      videoCanvasElement.height = videoHeight;
 
-        videoCanvasCtx.save();
-        videoCanvasCtx.clearRect(0, 0, videoCanvasElement.width, videoCanvasElement.height);
-        videoCanvasCtx.drawImage(results.image, 0, 0, videoCanvasElement.width, videoCanvasElement.height);
+      videoCanvasCtx.save();
+      videoCanvasCtx.clearRect(0, 0, videoCanvasElement.width, videoCanvasElement.height);
+      videoCanvasCtx.drawImage(results.image, 0, 0, videoCanvasElement.width, videoCanvasElement.height);
 
-        if (results.multiHandLandmarks) {
-          for (const landmarks of results.multiHandLandmarks) {
-            drawConnectors(videoCanvasCtx, landmarks, HAND_CONNECTIONS, {
-              color: '#00FF00',
-              lineWidth: 5,
-            });
-            drawLandmarks(videoCanvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
-          }
-        }
-
-        const tipIds = [4, 8, 12, 16, 20];
-        const fingUp = [0, 0, 0, 0, 0];
-        if (results.multiHandLandmarks.length !== 0) {
-          const temp = results.multiHandLandmarks[0];
-          const x1 = temp[8].x  * videoWidth-(2*videoWidth/3);
-          const y1 = temp[8].y * 2 * videoHeight;
-
-          const adjustedX1 = videoWidth - x1;
-          const adjustedX = adjustedX1 * 1.5 - videoWidth / 3;
-          const adjustedY = y1;  // Adjust x coordinate for mirrored video
-          console.log(adjustedX1);
-
-          if (temp[tipIds[0]].x > temp[tipIds[0] - 1].x) {
-            fingUp[0] = 1;
-          }
-
-          for (let i = 1; i < 5; i++) {
-            if (temp[tipIds[i]].y < temp[tipIds[i] - 2].y) {
-              fingUp[i] = 1;
-            }
-          }
-
-          // Cursor move mode when all five fingers are up
-          if (fingUp.every(finger => finger === 1)) {
-            cursorRef.current!.style.left = `${adjustedX1}px`;
-            cursorRef.current!.style.top = `${y1}px`;
-            shouldDraw.current = false;
-            dispatch(actionItemClick(MENU_ITEMS.PENCIL)); // Set to cursor move mode
-          } else if (fingUp[1] === 1 && fingUp[2] === 1) {
-            // Eraser mode when both index and middle fingers are up
-            const canvas = canvasRef.current;
-            const context = canvas!.getContext('2d');
-
-            context!.globalCompositeOperation = 'destination-out';
-            context!.lineWidth = (size ?? 1) * 2; // Make eraser larger
-            shouldDraw.current = true;
-            dispatch(actionItemClick(MENU_ITEMS.ERASER)); // Set to eraser mode
-          } else if (fingUp[1] === 1 && fingUp[2] === 0) {
-            // Normal drawing mode
-            const canvas = canvasRef.current;
-            const context = canvas!.getContext('2d');
-
-            if (shouldDraw.current) {
-              context!.lineTo(adjustedX, adjustedY);
-              context!.stroke();
-              context!.beginPath();
-              context!.moveTo(adjustedX, adjustedY);
-            } else {
-              shouldDraw.current = true;
-              context!.beginPath();
-              context!.moveTo(adjustedX, adjustedY);
-            }
-            context!.globalCompositeOperation = 'source-over';
-            context!.lineWidth = size ?? 1;
-            dispatch(actionItemClick(MENU_ITEMS.PENCIL)); // Set to drawing mode
-          } else {
-            // Normal drawing mode
-            const canvas = canvasRef.current;
-            const context = canvas!.getContext('2d');
-            context!.globalCompositeOperation = 'source-over';
-            context!.lineWidth = size ?? 1;
-            dispatch(actionItemClick(MENU_ITEMS.PENCIL)); // Set to drawing mode
-          }
-        }
-
-        videoCanvasCtx.restore();
-      });
-
-      if (webcamRef.current) {
-        try {
-          const camera = new cam.Camera(webcamRef.current.video!, {
-            onFrame: async () => {
-              await handpose.send({ image: webcamRef.current!.video! });
-            },
-            width: window.innerWidth,
-            height: window.innerHeight,
+      if (results.multiHandLandmarks) {
+        for (const landmarks of results.multiHandLandmarks) {
+          drawConnectors(videoCanvasCtx, landmarks, HAND_CONNECTIONS, {
+            color: '#00FF00',
+            lineWidth: 5,
           });
-          camera.start();
-          setWebcamAllowed(true);
-        } catch (error) {
-          console.error('Error accessing webcam:', error);
-          setWebcamAllowed(false);
+          drawLandmarks(videoCanvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
         }
       }
-    };
 
-    setupHandDetection();
+      const tipIds = [4, 8, 12, 16, 20];
+      const fingUp = [0, 0, 0, 0, 0];
+      if (results.multiHandLandmarks.length !== 0) {
+        const temp = results.multiHandLandmarks[0];
+        const x1 = temp[8].x  * videoWidth-(2*videoWidth/3);
+        const y1 = temp[8].y * 2 * videoHeight;
+        
+        const adjustedX1 = videoWidth - x1;
+        const adjustedX = adjustedX1 * 1.5 - videoWidth / 3;
+        const adjustedY = y1;  // Adjust x coordinate for mirrored video
+        console.log(adjustedX1);
+        
+        if (temp[tipIds[0]].x > temp[tipIds[0] - 1].x) {
+          fingUp[0] = 1;
+        }
+
+        for (let i = 1; i < 5; i++) {
+          if (temp[tipIds[i]].y < temp[tipIds[i] - 2].y) {
+            fingUp[i] = 1;
+          }
+        }
+
+        // Cursor move mode when all five fingers are up
+        if (fingUp.every(finger => finger === 1)) {
+          cursorRef.current!.style.left = `${adjustedX1}px`;
+          cursorRef.current!.style.top = `${y1}px`;
+          shouldDraw.current = false;
+          dispatch(actionItemClick(MENU_ITEMS.PENCIL)); // Set to cursor move mode
+        } else if (fingUp[1] === 1 && fingUp[2] === 1) {
+          // Eraser mode when both index and middle fingers are up
+          const canvas = canvasRef.current;
+          const context = canvas!.getContext('2d');
+
+          context!.globalCompositeOperation = 'destination-out';
+          context!.lineWidth = (size ?? 1) * 2; // Make eraser larger
+          shouldDraw.current = true;
+          dispatch(actionItemClick(MENU_ITEMS.ERASER)); // Set to eraser mode
+        } else if (fingUp[1] === 1 && fingUp[2] === 0) {
+          // Normal drawing mode
+          const canvas = canvasRef.current;
+          const context = canvas!.getContext('2d');
+
+          if (shouldDraw.current) {
+            context!.lineTo(adjustedX, adjustedY);
+            context!.stroke();
+            context!.beginPath();
+            context!.moveTo(adjustedX, adjustedY);
+          } else {
+            shouldDraw.current = true;
+            context!.beginPath();
+            context!.moveTo(adjustedX, adjustedY);
+          }
+          context!.globalCompositeOperation = 'source-over';
+          context!.lineWidth = size ?? 1;
+          dispatch(actionItemClick(MENU_ITEMS.PENCIL)); // Set to drawing mode
+        } else {
+          // Normal drawing mode
+          const canvas = canvasRef.current;
+          const context = canvas!.getContext('2d');
+          context!.globalCompositeOperation = 'source-over';
+          context!.lineWidth = size ?? 1;
+          dispatch(actionItemClick(MENU_ITEMS.PENCIL)); // Set to drawing mode
+        }
+      }
+
+      videoCanvasCtx.restore();
+    });
+
+    if (webcamRef.current) {
+      const camera = new cam.Camera(webcamRef.current.video!, {
+        onFrame: async () => {
+          await handpose.send({ image: webcamRef.current!.video! });
+        },
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+      camera.start();
+    }
   }, [dispatch, size]);
 
   return (
     <div>
-      {webcamAllowed && (
-        <Webcam hidden
-          ref={webcamRef}
-          onLoadedMetadata={() => {
-            if (webcamRef.current && videoCanvasRef.current) {
-              const videoWidth = webcamRef.current.video?.videoWidth || window.innerWidth;
-              const videoHeight = webcamRef.current.video?.videoHeight || window.innerHeight;
-              videoCanvasRef.current.width = videoWidth;
-              videoCanvasRef.current.height = videoHeight;
-            }
-          }}
-          style={{
-            position: 'absolute',
-            left: 0,
-            top: 0,
-            width: '100vw',
-            height: '100vh',
-            zIndex: 1, // Lower z-index to ensure other elements appear on top
-            transform: 'scaleX(-1)',
-          }}
-        />
-      )}
-      {webcamAllowed && (
-        <canvas
-          ref={videoCanvasRef}
-          style={{
-            position: 'absolute',
-            left: 0,
-            top: 0,
-            width: '100vw',
-            height: '100vh',
-            zIndex: 0, // Lower z-index to ensure other elements appear on top
-            transform: 'scaleX(-1)',
-            display: showVideoCanvas ? 'block' : 'none',
-          }}
-        />
-      )}
+      <Webcam hidden
+        ref={webcamRef}
+        onLoadedMetadata={() => {
+          if (webcamRef.current && videoCanvasRef.current) {
+            const videoWidth = webcamRef.current.video?.videoWidth || window.innerWidth;
+            const videoHeight = webcamRef.current.video?.videoHeight || window.innerHeight;
+            videoCanvasRef.current.width = videoWidth;
+            videoCanvasRef.current.height = videoHeight;
+          }
+        }}
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: 1, // Lower z-index to ensure other elements appear on top
+          transform: 'scaleX(-1)',
+        }}
+      />
+      <canvas
+        ref={videoCanvasRef}
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: 0, // Lower z-index to ensure other elements appear on top
+          transform: 'scaleX(-1)',
+          display: showVideoCanvas ? 'block' : 'none',
+        }}
+      />
       <canvas
         ref={canvasRef}
         style={{
